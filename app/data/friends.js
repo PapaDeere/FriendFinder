16ef46f84b53a16969796ba319a5609a5e5231af
Papa Deere <papadeere5150@gmail.com>
2018-02-10 11:12:57 -0700

    Add files via upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,6 @@
 //You should save your application's data inside of app/data/friends.js as an array of objects. 
+//You should save your application's data inside of app/data/friends.js as an array of objects. 
+//You should save your application's data inside of app/data/friends.js as an array of objects. 
 var friendsArray = [
 {
 	name: 'Tonka',
@@ -31,6 +33,186 @@ var friendsArray = [
 			'4',
 			'5',
 			]
+},
+{
+	name: 'Brutus',
+	photo: 'https://www.dogbreedinfo.com/images30/GreatPyreneesWorkingLinesVsShowLinesTacomaTundraFlockGuardians1.jpg',
+	scores: [
+			'1',
+			'2',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			]
+},			
+{
+	name: 'Jess',
+	photo: 'https://www.dogbreedinfo.com/images16/BloodhoundSophieSit.jpg',
+	scores: [
+			'1',
+			'2',
+			'2',
+			'2',
+			'2',
+			'1',
+			'2',
+			'2',
+			'2',
+			'1',
+			]
+},
+{
+	name: 'Miss Kitty',
+	photo: 'http://azdoberescue.org/wp-content/flagallery/rico/webview/rico-update1.jpg',
+	scores: [
+			'2',
+			'2',
+			'3',
+			'2',
+			'3',
+			'2',
+			'2',
+			'3',
+			'2',
+			'3',
+			]
+},
+{
+	name: 'Lil Bit',
+	photo: 'http://www.dogbreedhealth.com/wp-content/uploads/2011/04/Yorkshire-Terrier.jpg',
+	scores: [
+			'3',
+			'2',
+			'3',
+			'3',
+			'3',
+			'3',
+			'2',
+			'3',
+			'3',
+			'3',
+			]
+},
+{
+	name: 'Chuck',
+	photo: 'http://images.mentalfloss.com/sites/default/files/styles/insert_main_wide_image/public/iStock_000065468875_Small.jpg',
+	scores: [
+			'3',
+			'2',
+			'3',
+			'4',
+			'3',
+			'3',
+			'2',
+			'3',
+			'4',
+			'3',
+			]
+},
+{
+	name: 'Pink',
+	photo: 'http://cdn.shopify.com/s/files/1/0994/0236/files/brittany-spaniel_large.jpg?v=1503878980',
+	scores: [
+			'3',
+			'4',
+			'3',
+			'4',
+			'5',
+			'3',
+			'4',
+			'3',
+			'4',
+			'5',
+			]
+},
+{
+	name: 'Charley',
+	photo: 'https://vetstreet-brightspot.s3.amazonaws.com/a6/8e31c09e9411e0a2380050568d634f/file/Chesapeake-Bay-Retriever-5-645mk062111.jpg',
+	scores: [
+			'4',
+			'4',
+			'3',
+			'4',
+			'5',
+			'4',
+			'4',
+			'3',
+			'4',
+			'5',
+			]
+},
+{
+	name: 'Flipper',
+	photo: 'http://bruiserbulldogs.com/wp-content/uploads/2016/05/Attomwebsite3-Copy-850x526.jpg',
+	scores: [
+			'5',
+			'5',
+			'5',
+			'4',
+			'5',
+			'5',
+			'5',
+			'5',
+			'4',
+			'5',
+			]
+},			
+];
+module.exports = friendsArray;
+
+//Each of these objects should roughly follow the format below.
+//Determine the user's most compatible friend using the following as a guide:
+
+//Convert each user's results into a simple array of numbers 
+///(ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+//With that done, compare the difference between current user's scores against those from other users, 
+//question by question. Add up the differences to calculate the totalDifference.
+
+//Example: 
+//User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+//User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+//Total Difference: 2 + 1 + 2 = 5
+
+
+//Remember to use the absolute value of the differences. Put another way: no negative solutions! 
+//Your app should calculate both 5-3 and 3-5 as 2, and so on. 
+//The closest match will be the user with the least amount of difference.
+
+//Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+
+//The modal should display both the name and picture of the closest match. 
+
+
+module.exports = friendsArray;
+
+//Each of these objects should roughly follow the format below.
+//Determine the user's most compatible friend using the following as a guide:
+
+//Convert each user's results into a simple array of numbers 
+///(ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+//With that done, compare the difference between current user's scores against those from other users, 
+//question by question. Add up the differences to calculate the totalDifference.
+
+//Example: 
+//User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+//User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+//Total Difference: 2 + 1 + 2 = 5
+
+
+//Remember to use the absolute value of the differences. Put another way: no negative solutions! 
+//Your app should calculate both 5-3 and 3-5 as 2, and so on. 
+//The closest match will be the user with the least amount of difference.
+
+//Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+
+//The modal should display both the name and picture of the closest match. 
+
+
 },
 ];
 module.exports = friendsArray;
